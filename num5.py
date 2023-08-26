67b6ace5e6dcfd3b569e643c1859879f29d17c1c
anshitaa16 <142668368+anshitaa16@users.noreply.github.com>
2023-08-26 21:35:15 +0530

    Add files via upload

---
+++ num5.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+start = 30
+end = 71  # We use 71 to include 70 in the range
+step = 1
+
+array = np.arange(start, end, step)
+
+print("Array of integers from 30 to 70:", array)
